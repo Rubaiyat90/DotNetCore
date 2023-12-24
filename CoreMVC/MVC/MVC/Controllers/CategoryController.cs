1944c4e72cfd04563f48494cc9ce10cf9279f554
Rubaiyat Rahman <94232498+Rubaiyat90@users.noreply.github.com>
2023-12-24 22:08:45 +0600

    Initial Uplaod

---
+++ CoreMVC/MVC/MVC/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+        
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> ctrList = _db.Categories.ToList();
+            return View(ctrList);
+        }
+    }
+}
