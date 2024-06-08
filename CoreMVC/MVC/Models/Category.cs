0830edba96d821bc7700814fd7f24eb3ae6208b5
Rubaiyat Rahman <94232498+Rubaiyat90@users.noreply.github.com>
2024-06-08 22:29:06 +0600

    ModelsAdded

---
+++ CoreMVC/MVC/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Range(1,100,ErrorMessage ="Range must be between 1 to 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
